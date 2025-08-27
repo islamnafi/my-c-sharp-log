0f44f20e1fe8d3956d7ff633c8b22945db2ad262
Nafi <152697412+islamnafi@users.noreply.github.com>
2025-08-27 17:16:49 +0600

    variables

---
+++ basics/variables/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            userName = Console.ReadLine();
+            Console.WriteLine("Hi " + userName);
+
+            int score;
+            score = 53;
+            score = 54;
+            Console.WriteLine(score); // Outputs: 54
+
+            int a = 5;
+            int b = 6;
+
+            b = a;
+
+            Console.WriteLine(a); // Outputs: 5
+            Console.WriteLine(b); // Outputs: 5
+        }
+    }
+}
