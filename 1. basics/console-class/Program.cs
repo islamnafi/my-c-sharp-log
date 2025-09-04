ba5f0fe896367742d9b27e479a911b2f3661792b
Nafi <152697412+islamnafi@users.noreply.github.com>
2025-09-04 18:39:56 +0600

    beginning of an ending

---
+++ 1. basics/console-class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear(); //Console.Clear() wipes the screen and fills the entire buffer with the current background color, then moves the cursor to the top-left.
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
